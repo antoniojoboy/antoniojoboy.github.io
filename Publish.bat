0d91db9a0d4751a8c6407e2fe5bef7e0219a5a55
antoniojoboy <joboyantonio@gmail.com>
2021-06-07 14:17:05 +0800

    zf

--- Publish.bat
+++ Publish.bat
@@ -2,6 +2,7 @@ pelican content -o output -s publishconf.py
 ghp-import -m "Generate Pelican site" --no-jekyll -b main output
 git push origin main
 git add content
-git commit -m "added a first post, a photo and an about page"
+git commit -m "added a first post, a photo and an about pages"
 git push origin content
-start firefox.exe antoniojoboy.github.io
+pause
+@REM start firefox.exe antoniojoboy.github.io
